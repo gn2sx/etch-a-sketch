621e8c5e0097304c01d28220cb2f685f31a685b3
Ryan H. Jewell <rjewelld@gmail.com>
2017-03-14 21:26:26 -0400

    Added buttons, and rainbow/eraser functionality

--- script.js
+++ script.js
@@ -12,14 +12,44 @@ function drawGrid(size){
   $('.square').css('height',squareSize+"px");
   $('.square').css('width',squareSize+"px");
 }
+var color='black';
+var eraserOn=false;
+var rainbowOn=false;
+var rainbowColors=['red','orange','yellow','green','blue','indigo','violet'];
 $(document).ready(function(){
+  var gridOn=true;
   drawGrid(16);
   $('#clear').click(function(){//clears and resizes the grid
     var gridSize=window.prompt("How large should the sides of the new grid be?",16);
     drawGrid(gridSize);
   });
+  $('#eraser').click(function(){
+    if(eraserOn){
+      color='black';
+      eraserOn=false;
+    }
+    else{
+      color='white';
+      eraserOn=true;
+      rainbowOn=false;
+    }
+  });
+  $('#rainbow').click(function(){
+    if(rainbowOn){
+      color='black';
+      rainbowOn=false;
+    }
+    else{
+      color=rainbowColors[Math.floor(Math.random()*rainbowColors.length)]; //randomly pulls a color form RainbowColors
+      rainbowOn=true;
+      eraserOn=false;
+    }
+  });
 });
 
-$(document).on('mouseenter','.square',function(){ //Turns squares red as mouse passes over them
-  $(this).css('background-color','red');
+$(document).on('mouseenter','.square',function(){ //Turns squares colors as mouse passes over them
+  $(this).css('background-color',color);
+  if(rainbowOn){
+    color=rainbowColors[Math.floor(Math.random()*rainbowColors.length)];//assings a new random color if RainbowOn is true
+  }
 });
